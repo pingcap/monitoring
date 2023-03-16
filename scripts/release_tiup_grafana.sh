f0301c18ad0a4dc572674cb245c2b340f5afad23
lijie <lijie@pingcap.com>
2023-03-16 17:32:57 +0800

    fix: on mac arm
    Signed-off-by: lijie <lijie@pingcap.com>

--- scripts/release_tiup_grafana.sh
+++ scripts/release_tiup_grafana.sh
@@ -1,7 +1,11 @@
 set -ex
 BASEDIR="$(dirname -- "$0")/.."
 rm -f grafana.tar.gz
-wget -O grafana.tar.gz -qnc https://download.pingcap.org/grafana-7.5.11.$OS-$ARCH.tar.gz
+if [ $OS = 'darwin' ] && [ $ARCH = 'arm64' ] ; then
+	wget -O grafana.tar.gz -qnc  http://fileserver.pingcap.net/download/pingcap/grafana-7.5.10.darwin-arm64.tar.gz
+else
+	wget -O grafana.tar.gz -qnc https://download.pingcap.org/grafana-7.5.11.$OS-$ARCH.tar.gz
+fi
 mkdir -p grafana
 tar -C grafana --strip-components=1 -xzf grafana.tar.gz
 cp $BASEDIR/output/tiup_dashboards/* grafana/
