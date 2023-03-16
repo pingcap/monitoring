05c02e5a0dd7b342e4f6d717e643e5d583f22e88
lijie <lijie@pingcap.com>
2023-03-16 16:15:27 +0800

    feat: basedir
    Signed-off-by: lijie <lijie@pingcap.com>

--- scripts/release_tiup_grafana.sh
+++ scripts/release_tiup_grafana.sh
@@ -1,8 +1,9 @@
 set -ex
+BASEDIR="$(dirname -- "$0")/.."
 rm -f grafana.tar.gz
 wget -O grafana.tar.gz -qnc https://download.pingcap.org/grafana-7.5.11.$OS-$ARCH.tar.gz
 mkdir -p grafana
 tar -C grafana --strip-components=1 -xzf grafana.tar.gz
-cp tiup_dashboards/* grafana/
+cp $BASEDIR/output/tiup_dashboards/* grafana/
 tar -C grafana -czf grafana.tar.gz .
 tiup mirror publish grafana ${VERSION} grafana.tar.gz "bin/grafana-server" --arch $ARCH --os $OS --desc="Grafana is the open source analytics & monitoring solution for every database"
