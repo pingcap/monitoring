970075f4a3feb0f1e6f0c335bc185a7eb3d4a6d0
lijie <lijie@pingcap.com>
2023-03-16 15:18:44 +0800

    feat: generate tiup grafana package
    Signed-off-by: lijie <lijie@pingcap.com>

---
+++ scripts/release_tiup_grafana.sh
@@ -0,0 +1,11 @@
+set -e
+set -o xtrace
+SCRIPTDIR=$(dirname -- "$0")
+cd $SCRIPTDIR/../output
+rm -f grafana.tar.gz
+wget -O grafana.tar.gz -qnc https://download.pingcap.org/grafana-7.5.11.$OS-$ARCH.tar.gz
+mkdir -p grafana
+tar -C grafana --strip-components=1 -xzf grafana.tar.gz
+cp tiup_dashboards/* grafana/
+tar -C grafana -czf grafana.tar.gz .
+tiup mirror publish grafana ${VERSION} grafana.tar.gz "bin/grafana-server" --arch $ARCH --os $OS --desc="Grafana is the open source analytics & monitoring solution for every database"
