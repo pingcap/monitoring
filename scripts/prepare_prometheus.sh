e9b143300406a6da5d821df004e293696fe37d31
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2024-03-01 19:26:01 +0800

    ci: fix prepare_prometheus.sh (#311)
    do not rm the outout folder
    
    Co-authored-by: wuhuizuo <wuhuizuo@126.com>

--- scripts/prepare_prometheus.sh
+++ scripts/prepare_prometheus.sh
@@ -33,10 +33,10 @@ main() {
     ref_dir="$(get_ref_path)"
     echo "Ref directory is: $ref_dir"
 
-    rm -rf "$archive_dir"
     mkdir -p "$archive_dir"
 
     ## Compose prometheus files from the community repo.
+    rm -rf ${archive_dir}/prometheus
     prometheus_dirname="prometheus-${prometheus_ver}.${prometheus_os}-${prometheus_arch}"
     prometheus_download_url="https://download.pingcap.org/${prometheus_dirname}.tar.gz"
     wget "$prometheus_download_url" -O - | tar -zxvf - --strip-components=0 -C "$archive_dir" "$prometheus_dirname"
