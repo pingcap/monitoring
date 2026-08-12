9342263f8f012de9fb2391d74ed19fc4caece774
Zhou Kunqin <25057648+time-and-fate@users.noreply.github.com>
2026-08-12 11:35:28 +0800

    build: apply Go dependency security updates to master (#1159)
    * build: bump vulnerable Go dependencies
    
    (cherry picked from commit b15ed01988bd2650c8de0eafbc482254d98752b3)
    
    * build: fix rulefmt.Parse API usage missed in cmd/main.go
    
    The prometheus v0.55->v0.312 bump changed the rulefmt.Parse signature and
    RuleNode->Rule (plain string fields). cmd/main.go was not updated, causing
    the govulncheck/compile failure reported in review. Also fixes a latent
    RuleNode cast in pkg/operator/rules.go that would panic at runtime.
    
    (cherry picked from commit 35f777f964f49ee7fffc983093c4fe15aa8ce98b)
    
    * build: upgrade Go and x/text
    
    * build: keep floating Go 1.25 image tag
    
    ---------
    
    Co-authored-by: Victor Tatai <vtatai@atlassian.com>

--- pkg/operator/rules.go
+++ pkg/operator/rules.go
@@ -2,12 +2,14 @@ package operator
 
 import (
 	"errors"
+	"log/slog"
 	"strings"
 	"time"
 
 	"github.com/pingcap/monitoring/pkg/common"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/model/rulefmt"
+	"github.com/prometheus/prometheus/promql/parser"
 	"github.com/youthlin/stream"
 	streamtypes "github.com/youthlin/stream/types"
 	"gopkg.in/yaml.v3"
@@ -32,7 +34,7 @@ func WriteRule(body string, ruleName string, baseDir string, needToReplaceExpr m
 }
 
 func replaceAlertExpr(content []byte, needToReplaceExpr map[string]string) ([]byte, error) {
-	groups, errs := rulefmt.Parse(content)
+	groups, errs := rulefmt.Parse(content, false, model.NameValidationScheme, parser.NewParser(parser.Options{}), slog.Default())
 	if len(errs) > 0 {
 		return nil, errors.Join(errs...)
 	}
@@ -42,30 +44,30 @@ func replaceAlertExpr(content []byte, needToReplaceExpr map[string]string) ([]by
 		newG := rulefmt.RuleGroup{
 			Interval: group.Interval,
 			Name:     group.Name,
-			Rules:    make([]rulefmt.RuleNode, 0, len(group.Rules)),
+			Rules:    make([]rulefmt.Rule, 0, len(group.Rules)),
 		}
 
 		stream.OfSlice(group.Rules).Map(func(t streamtypes.T) streamtypes.R {
-			rule := t.(rulefmt.RuleNode)
+			rule := t.(rulefmt.Rule)
 			if time.Duration(rule.For) <= (time.Second * 60) {
 				if err != nil {
 					rule.For = forConfig
 				}
 			}
 
-			newExpr, ok := needToReplaceExpr[strings.ToUpper(rule.Alert.Value)]
+			newExpr, ok := needToReplaceExpr[strings.ToUpper(rule.Alert)]
 			if !ok {
 				return rule
 			}
 
-			rule.Expr.SetString(newExpr)
+			rule.Expr = newExpr
 			if _, ok := rule.Labels["expr"]; ok {
 				rule.Labels["expr"] = newExpr
 			}
 
 			return rule
 		}).ForEach(func(t streamtypes.T) {
-			rule := t.(rulefmt.RuleNode)
+			rule := t.(rulefmt.Rule)
 			newG.Rules = append(newG.Rules, rule)
 		})
 
